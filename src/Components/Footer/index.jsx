f099f72a75f31a3be80bc0df2d53c5986caed92d
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-09-23 13:10:36 -0700

    added files i deleted like a ding-dong

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>&copy; 2018</footer>
+  )
+}
+
+export default Footer
+
+
+
+
+// import React from 'react';
+
+// class Footer extends React.Component {
+//   render() {
+//     return <footer>&copy; 2018</footer>;
+//   }
+// }
+
+// export default Footer;
