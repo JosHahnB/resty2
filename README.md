2e405458caa9f924c482bde164df363be461ff37
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-09-30 15:31:40 -0700

    completed lab 27

--- README.md
+++ README.md
@@ -6,3 +6,31 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Functional requirements
+
+Extend your React Application so that your functional components are able to manage their own state variables using the `useState()` Hook.
+NOTE: It is not a requirement to make the actual API call. That can be mocked with “fake” data.
+
+Refactor any components using this.setState() to implement the useState() react API hook.
+Refactor the Form Component to implement user input from form elements, instead of hard coded string values.
+Suggested Component Hierarchy and Application Architecture:
+
+index.js - Entry Point.
+`<App />` - Container.
+Holds application state: The Request (from the form) and the Response (from the API).
+Hook that can update state.
+Renders 2 Child Components.
+`<Form />`
+Expects a function to be sent to it as a prop.
+Renders a URL entry form.
+A selection of REST methods to choose from (“get” should be the default).
+The active selection should be displayed/styled differently than the others.
+Renders a Textarea to allow the user to type in a JSON object for a POST or PUT request.
+On submit:
+Send the Form entries back to the `<App />` using the method sent down in props.
+Form will run the API request.
+Toggle the “loading” status before and after the request.
+`<Results />`
+Conditionally renders “Loading” or the data depending on the status of the request.
+Renders the data as “pretty” JSON.
--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,71 +1,93 @@
-import {useState} from "react";
+import { useState } from "react";
 import "./Form.scss";
-import '../../App'
 
 const Form = (props) => {
-  const {appState, setAppState} = props;
-  const [formData, setFormData] = useState ({
-    method: 'GET',
-    url:'https://pokeapi.co/api/v2/pokemon',
+  const [formData, setFormData] = useState({
+    method: "GET",
+    url: "",
+    body: "",
   });
+
   const handleSubmit = (e) => {
     e.preventDefault();
     props.handleApiCall(formData);
-  }
-    const handleOnFormChange =(e) => {
-      method: "GET",
-      props.handleApiCall(formData);
-    };
-  }
+  };
+
+  const handleFormInput = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleButtonClick = (e) => {
+    setFormData({ ...formData, method: e.target.id.toUpperCase() });
+  };
+
   return (
     <>
       <form onSubmit={handleSubmit}>
         <label>
           <span>URL: </span>
-          <input name="url" type="text" />
-          <button type="submit">GO!</button>
+          <input
+            data-testid="formInput"
+            name="url"
+            type="text"
+            value={formData.url}
+            onChange={handleFormInput}
+          />
+          <button data-testid="goButton" style={{ color: "red" }} type="submit">
+            GO!
+          </button>
         </label>
         <label className="methods">
-          <span id="get">GET</span>
-          <span id="post">POST</span>
-          <span id="put">PUT</span>
-          <span id="delete">DELETE</span>
+          <button
+            className="method"
+            id="get"
+            type="button"
+            onClick={handleButtonClick}
+          >
+            GET
+          </button>
+          <button
+            className="method"
+            id="post"
+            type="button"
+            onClick={handleButtonClick}
+          >
+            POST
+          </button>
+          <button
+            className="method"
+            id="put"
+            type="button"
+            onClick={handleButtonClick}
+          >
+            PUT
+          </button>
+          <button
+            className="method"
+            id="delete"
+            type="button"
+            onClick={handleButtonClick}
+          >
+            DELETE
+          </button>
         </label>
-        {
-          (formData.method === 'post'|| FormDataEvent.method === 'put') && <h1>change this to a text area</h1>
-          // make this a html text area. make state to be value of the txt area ('body')?
-          // create onChangeHandler for the text are that updates this.state.can copy setState for form data. Change body instead of dformData, body may be string or object
-        }
+        {["PUT", "POST"].includes(formData.method) && (
+          <textarea
+            value={formData.body}
+            rows={10}
+            onChange={handleFormInput}
+            name="body"
+          />
+        )}
+        {/* {(formData.method === "POST" || formData.method === "PUT") && (
+          <textarea />
+        )} */}
+        {/* {myArray.length && {"BAD"}}
+        {myArray.length > 0 && {"GOOD"}} */}
       </form>
     </>
   );
 };
 
 export default Form;
-
-// import React from 'react';
-
-// class Form extends React.Component {
-
-//   render() {
-//     return (
-// <>
-//   <form onSubmit={this.handleSubmit}>
-//     <label >
-//       <span>URL: </span>
-//       <input name='url' type='text' />
-//       <button type="submit">GO!</button>
-//     </label>
-//     <label className="methods">
-//       <span id="get">GET</span>
-//       <span id="post">POST</span>
-//       <span id="put">PUT</span>
-//       <span id="delete">DELETE</span>
-//     </label>
-//   </form>
-// </>
-//     );
-//   }
-// }
-
-// export default Form;
