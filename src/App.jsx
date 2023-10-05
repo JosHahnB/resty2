35c59fcf482d204202a7f852d4824d426ef3c28e
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-10-04 20:50:54 -0700

    added some starter code

--- src/App.jsx
+++ src/App.jsx
@@ -1,115 +1,77 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.scss';
 
-import "./App.scss";
 
 // Let's talk about using index.js and some other name in the component folder.
 // There's pros and cons for each way of doing this...
-// OFFICIALLY, we have chosen to use the Airbnb style guide naming convention.
+// OFFICIALLY, we have chosen to use the Airbnb style guide naming convention. 
 // Why is this source of truth beneficial when spread across a global organization?
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import Form from "./Components/Form";
-import Results from "./Components/Results";
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Form from './Components/Form';
+import Results from './Components/Results';
 
 const App = () => {
   const [appState, setAppState] = useState({
-    data: {},
-    requestParams: {},
-  });
+        data: {},
+        requestParams: {},
+      });
 
-  // listen for the state (appState.requestParams) to change (url and method, maybe body)
-  // when they change, make a http request
-  // update the DATA (appState.data) to the new values
-  // use a useEffect to do this
+      const History = () => {
+        const [appState, dispatch] = useReducer(reduceFunction, initialValue)
 
-  // useEffect is a hook, it takes 2 arguments: callback function, dependancy array
-  // OPTION 1
-  // useEffect(() => {
-  //   // can do anything
-  //   // this function runs on component mount AND when any dependancies in the array change
-  //   if (!appState.requestParams.url) return;
-  //   (async () => {
-  //     // make the request
-  //     const url = appState.requestParams.url;
-  //     const method = appState.requestParams.method;
-  //     // make the request, get back data
-  //     // spread opperator takes the object and spreads it apart
-  //     // {data, requestParams}
-  //     // {...appState, pizza: "yum"}
-  //     // {data, requestParams, pizza: "yum"}
-  //     // {appState, pizza: "yum"}
-  //     // {appState: {data, requestParams}, pizza: "yum"}
-  //     //{...appState, data: {}}
-  //     // {data: {fkjsdhfkafh" "woo"}, requestParams: {url: "yaya", method: "GET"}, data: {}}
-  //     setAppState((prev) => ({ ...prev, data: {} }));
-  //     // be very careful that you don't create a circular dependancy - where the state of the thing you are watching changes every time the function runs
-  //   })();
-  // }, [appState.requestParams]);
+        
+      }
 
-  // OPTION 2
-  useEffect(() => {
-    // can do anything
-    if (!appState.requestParams.url) return;
-    if (appState.data && Object.keys(appState.data).length) return;
-    // early exit gate
-    (async () => {
-      // make the request
-      const url = appState.requestParams.url;
-      const method = appState.requestParams.method;
-      console.log(url, method);
-      // make the request, get back data
-      const request = {
-        data: {
-          count: 2,
-          results: [
-            { name: "fake thing 1", url: "http://fakethings.com/1" },
-            { name: "fake thing 2", url: "http://fakethings.com/2" },
-          ],
-        },
-      };
-      // spread opperator takes the object and spreads it apart
-      // {data, requestParams}
-      // {...appState, pizza: "yum"}
-      // {data, requestParams, pizza: "yum"}
-      // {appState, pizza: "yum"}
-      // {appState: {data, requestParams}, pizza: "yum"}
-      //{...appState, data: {}}
-      // {data: {fkjsdhfkafh" "woo"}, requestParams: {url: "yaya", method: "GET"}, data: {}}
-      setAppState({ ...appState, data: request.data });
-      // be very careful that you don't create a circular dependancy - where the state of the thing you are watching changes every time the function runs
-    })();
-    return () => {
-      console.log("component unmounts");
-    };
-    // the dependancy array is our watch list
-  }, [appState]);
+
+      useEffect(() => {
+      // //   // can do anything
+        if(!appState.requestParams.url) return;
+        if(appState.data && Object.keys(appState.data).length) return;
+        console.log('made it!');
+        (async () => {
+          const url = appState.requestParams.url;
+          const method = appState.requestParams.method;
+          console.log(url, method);
+          // make the request to get back data
+          // const request = 
+            // async () => {
+              const { data } = await axios.get(appState.requestParams.url);
+              console.log(appState);
+              setAppState({...appState, data});
+            
+          // ***SPREAD OPERATORS!
+          // spread operator takes the object and spreads it apart
+          // {data, requestParams}
+          // saying {...appState, pizza: 'yum'} means: 
+          // {data, requestParams, pizza: 'yum' }
+          // setAppState({...appState, data: request.data});
+        })();
+        return () => {
+          console.log('component unmounts');
+        };
+          // be careful that you don't create a circular dependency 
+          // where the state of the thing you're watching changes everytime the function runs
+        
+      }, [appState]);
 
   const callApi = (requestParams) => {
-    // mock output
-    // const data = {
-    //   count: 2,
-    //   results: [
-    //     { name: "fake thing 1", url: "http://fakethings.com/1" },
-    //     { name: "fake thing 2", url: "http://fakethings.com/2" },
-    //   ],
-    // };
-    // updating the requestParams in state will trigger the use effect
-    setAppState({ data: {}, requestParams });
-  };
+    setAppState({data: {}, requestParams});
+  }
 
-  return (
-    <React.Fragment>
-      <Header />
-      <div>Request Method: {appState.requestParams.method}</div>
-      <div>URL: {appState.requestParams.url}</div>
-      {appState.requestParams.body && (
-        <div>Body: {appState.requestParams.body}</div>
-      )}
-      <Form handleApiCall={callApi} />
-      <Results data={appState.data} />
-      <Footer />
-    </React.Fragment>
-  );
-};
+    return (
+      <React.Fragment>
+        <Header />
+        <div>Request Method: {appState.requestParams.method}</div>
+        <div>URL: {appState.requestParams.url}</div>
+        {/* {appState.requestParams.body} &&
+        <div>appState.requestParams.body</div> */}
+        <Form handleApiCall={callApi} />
+    {Object.keys(appState.data).length > 0 && <Results data={appState.data} />}        
+    <Footer />
+      </React.Fragment>
+    );
+}
 
 export default App;
